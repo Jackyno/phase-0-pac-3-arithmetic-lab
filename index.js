0fbf20794411ec77af7424a9b783dcee622d7866
JackynoJEANBAPTISTE <jackynojeanbaptiste@gmail.com>
2025-04-03 13:45:09 -0400

    arithmetic-lab-complete

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+    return "something";
+  }
+
+  function subtract() {
+    return "something";
+  }
+
+  function multiply() {
+    return "something";
+  }
+
+  function divide() {
+    return "something";
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function increment(n) {
+    return n += 1;
+  }
+
+  function decrement(n) {
+    return n -= 1;
+  }
+
+  function makeInt(string) {
+    return parseInt(string, 10); 
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string); 
+}
+
+
+
